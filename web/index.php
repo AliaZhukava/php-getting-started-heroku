b3afe4965bf1bc286b4a4e681a257a791cbc822a
John Doe <johndoe@example.com>
2017-10-04 21:03:16 +0300

    yrgdfg

--- web/index.php
+++ web/index.php
@@ -24,6 +24,20 @@
       <input id="my-radio-yes" name="radio" type="radio" required value="Yes">
       <label for="my-radio-no">radio No: </label>
       <input id="my-radio-no" name="radio" type="radio" required value="No"></p>
+      <p><label for="my-color">color: </label>       <input id="my-color" type="color"></p>
+      <p><label for="my-date">date: </label>         <input id="my-date" type="date" required></p>
+      <p><label for="my-month">month: </label>       <input id="my-month" type="month" required></p>
+      <p><label for="my-email">email: </label>       <input id="my-email" type="email" required></p>
+      <p><label for="my-number">number: </label>     <input id="my-number" type="number"></p>
+      <p><label for="my-range">range: </label>       <input id="my-range" type="range" min="0" max="10"></p>
+      <p><label for="my-checkbox">checkbox: </label> <input id="my-checkbox" type="checkbox" checked></p>
+      <p><label for="my-select">select: </label>     <select id="my-select" size="1" required>
+      <option value="">---------</option>
+      <option value="1">Первая</option>
+      <option value="2" selected>Вторая</option>
+      </select></p>
+      <textarea id="my-textarea" style="resize:none" placeholder="Введите многострочный текст"></textarea><br>
+
     <br>
     <input type="submit" value="Submit">
     <input type="reset" value="Clear">
