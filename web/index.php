5214df0f08dd037031ea48fa1c5da55906b6f31c
John Doe <johndoe@example.com>
2017-11-03 21:37:40 +0300

    new site

--- web/index.php
+++ web/index.php
@@ -1,58 +1,300 @@
-<!DOCTYPE html>
-<html lang="en" >
+<!DOCTYPE HTML>
+<html>
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Form test</title>
+<title>BeInShape Gym | Главная</title>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script src="js/jquery-1.11.1.min.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/responsiveslides.min.js"></script>
+ <script>
+    $(function () {
+      $("#slider").responsiveSlides({
+      	auto: true,
+      	nav: true,
+      	speed: 500,
+        namespace: "callbacks",
+        pager: true,
+      });
+    });
+  </script>
+	<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+	<script src="js/wow.min.js"></script>
+		<script>
+			new WOW().init();
+		</script>
 </head>
 <body>
+	<nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header wow fadeInLeft animated animated" data-wow-delay="0.4s">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Меню</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+         <h1><a class="navbar-brand" href="index.html"> BeInShape Gym</a></h1>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+			<ul class="nav navbar-nav navbar-right wow fadeInRight animated animated" data-wow-delay="0.4s">
+				<li class="active"><a class="nav-in" href="index.html"><span data-letters="Главная">Главная</span></a></li>
+				<li ><a class="nav-in" href="about.html"><span data-letters="О нас">О нас</span></a> </li>
+				<li><a class="nav-in" href="trainers.html"><span data-letters="Тренеры">Тренеры</span></a></li>
+				<li><a class="nav-in" href="photo.html"><span data-letters="Галерея">Галерея</span></a></li>
+				<li><a class="nav-in" href="contact.html"><span data-letters="Контакты">Контакты</span></a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+	<div class="banner-section">
+		<div class="container">
+			<div class="slider">
+				<div class="callbacks_container">
+					<ul class="rslides" id="slider">
+						 <li>	 
+							<div class="caption">
+							<div class="col-md-6 cap-left wow fadeInDownBig animated animated" data-wow-delay="0.4s">
+								<p>Добро пожаловать на официальный сайт нашего тренажерного зала BeInShape Gym!</p>
+							</div>
+							<div class="col-md-6 cap-right wow fadeInUpBig animated animated" data-wow-delay="0.4s">
+								<h3>Уверенность &mdash; твоя суперсила!</h3>
+							</div>
+							<div class="clearfix"></div>
+							</div>
+						</li>
+						 <li>	 
+							<div class="caption">
+							<div class="col-md-6 cap-left wow fadeInDownBig animated animated" data-wow-delay="0.4s">
+								<p>Не упустите свою возможность стать лучше! Если Ваша мечта &mdash; идеальное тело, то мы вместе можем ее осуществить!</p>
+							</div>
+							<div class="col-md-6 cap-right wow fadeInUpBig animated animated" data-wow-delay="0.4s">
+								<h3>Все в твоих руках. Главное &mdash; желание!</h3>
+							</div>
+								<div class="clearfix"></div>
+							</div>
+						 </li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+			<div class="content">
+					<div class="train w3-agile">
+						<div class="container">
+							<h2>Тренируйся с нами</h2>
+							<div class="train-grids">
+								<div class="col-md-3 train-grid wow fadeInLeft animated animated" data-wow-delay="0.4s">
+									<div class="train-top hvr-bounce-to-right">
+										<div class="train-img">
+											<img src="images/e1.png"/>
+										</div>
+										<h4>Групповые тренировки</h4>
+										<p>Это несомненная польза для здоровья в простой и приятной форме.</p>
+									</div>
+								</div>
+								<div class="col-md-3 train-grid wow fadeInDownBig animated animated" data-wow-delay="0.4s">
+									<div class="train-top hvr-bounce-to-right">
+										<div class="train-img">
+											<img src="images/e2.png"/>
+										</div>
+										<h4>Гантели</h4>
+										<p>В нашем зале большой запас гантелей от 0,5 до 30 кг. Каждый найдет свою пару на любой вес и цвет.</p>
+									</div>
+								</div>
+								<div class="col-md-3 train-grid wow fadeInUpBig animated animated" data-wow-delay="0.4s">
+									<div class="train-top hvr-bounce-to-right">
+										<div class="train-img">
+											<img src="images/e3.png"/>
+										</div>
+										<h4>Штанга</h4>
+										<p>Если Вы метите в тяжеловесы, то Вы всегда найдете в нашем зале поддержку и подстраховку</p>
+									</div>
+								</div>
+								<div class="col-md-3 train-grid wow fadeInRight animated animated" data-wow-delay="0.4s">
+								<div class="train-top hvr-bounce-to-right">
+										<div class="train-img">
+											<img src="images/e4.png"/>
+										</div>
+										<h4>Стрейчинг</h4>
+										<p>После тренировки обязательно нужно растянуть мышцы! Они будут Вам благодарны.</p>
+									</div>
+								</div>
+								<div class="clearfix"></div>
+							</div>
+							<div class="train-grids">
+								<div class="col-md-3 train-grid wow fadeInLeft animated animated" data-wow-delay="0.4s">
+									<div class="train-top hvr-bounce-to-right">
+										<div class="train-img">
+											<img src="images/e5.png"/>
+										</div>
+										<h4>Кардио</h4>
+										<p>При похудении никак не обойтись без кардиотренировок. У нас тренажеры на любой вкус.</p>
+									</div>
+								</div>
+								<div class="col-md-3 train-grid wow fadeInDownBig animated animated" data-wow-delay="0.4s">
+									<div class="train-top hvr-bounce-to-right">
+										<div class="train-img">
+											<img src="images/e6.png"/>
+										</div>
+										<h4>Подтягивания</h4>
+										<p>Подтягивайтесь на перекладине &mdash; тянитесь к вершинам и своей мечте! Все обязательно получится!</p>
+									</div>
+								</div>
+								<div class="col-md-3 train-grid wow fadeInUpBig animated animated" data-wow-delay="0.4s">
+									<div class="train-top hvr-bounce-to-right">
+										<div class="train-img">
+											<img src="images/e7.png"/>
+										</div>
+										<h4>Скакалка </h4>
+										<p>Прыжки со скакалкой тренируют дыхательную систему, укрепляют сердце и сосуды. </p>
+									</div>
+								</div>
+								<div class="col-md-3 train-grid wow fadeInRight animated animated" data-wow-delay="0.4s">
+									<div class="train-top hvr-bounce-to-right">
+										<div class="train-img">
+											<img src="images/e8.png"/>
+										</div>
+										<h4>Жим от груди </h4>
+										<p>Если вы хотите оформить мышцы груди – жим в тренажёре идеально Вам подойдёт.</p>
+									</div>
+								</div>
+								<div class="clearfix"></div>
+							</div>
+						</div>
+					</div>
+				<div class="fit-section w3l-layouts">
+					<div class="container">
+						<div class="fit-grids">
+							<div class="col-md-4 fit-grid wow fadeInDownBig animated animated" data-wow-delay="0.4s">
+								<div class="fit-left hvr-bounce-to-bottom">
+									<h3>Режим работы</h3>
+									<ul>
+									<li>Понедельник - Пятница : 09:00 - 20:00</li>
+									<li>Суббота : 09:00 - 16:00</li>
+									<li>Воскресенье : выходной</li>
+									</ul>
+								</div>
+							</div>
+							<div class="col-md-4 fit-grid wow fadeInUpBig animated animated" data-wow-delay="0.4s">
+								<img src="images/f2.jpg" class="img-responsive" alt=""/>
+							</div>
+							<div class="col-md-4 fit-grid wow fadeInRight animated animated" data-wow-delay="0.4s">
+								<div class="fit-right hvr-bounce-to-right">
+									<h3>Индивидуальные занятия с тренером</h3>
+									<p>Инструктор не только будет контролировать выполнение вами заданий, но и составит для вас личную программу. Он оценит вашу физическую форму, вычислит слабые и сильные места. Ваш личный тренер даст полезные рекомендации относительно рациона, чтобы вы максимально быстро начали замечать позитивные изменения в фигуре. </p>
+								</div>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+						<div class="fit-grids1">
+							<div class="clearfix"></div>
+						</div>
+						<div class="fit-grids wow fadeInLeft animated animated" data-wow-delay="0.4s">
+						<div class="col-md-4 fit-grid">
+								<img src="images/f1.jpg" class="img-responsive" alt=""/>
+							</div>
+							<div class="col-md-4 fit-grid wow fadeInDownBig animated animated" data-wow-delay="0.4s">
+								<div class="fit-left hvr-bounce-to-bottom">
+									<h3>Режим работы</h3>
+									<ul>
+									<li>Понедельник - Пятница : 09:00 - 20:00</li>
+									<li>Суббота : 09:00 - 16:00</li>
+									<li>Воскресенье : выходной</li>
+									</ul>
+								</div>
+							</div>
+							<div class="col-md-4 fit-grid wow fadeInRight animated animated" data-wow-delay="0.4s">
+								<div class="fit-right hvr-bounce-to-right">
+									<h3>CrossFit</h3>
+									<p>Это экстремальная система общей физической подготовки, основанная на чередующихся, иногда случайным образом, базовых движениях из различных видов спорта (гиревой спорт, тяжёлая атлетика, спортивная гимнастика, гребля, легкая атлетика и т.д.), выполняемых с высокой интенсивностью. Лучше самому попробовать. Приходите на бесплатную тренировку!</p>
+								</div>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+						<div class="fit-grids1">
+						<div class="col-md-4 fit-grid1 hvr-bounce-to-bottom wow fadeInDownBig animated animated" data-wow-delay="0.4s">
+								<div class="fit-right">
+									<h3>Fitness</h3>
+									<p>Тренажерный зал только для мужчин? Чушь! В нашем зале опытные тренеры подберут индивидуальный план тренировок подходящий каждой представительнице прекрасного пола. </p>
+								</div>
+							</div>
+							<div class="col-md-8 fit-grid2 wow fadeInUpBig animated animated" data-wow-delay="0.4s">
+								<img src="images/f4.jpg" class="img-responsive" alt=""/>
+							</div>
+							<div class="clearfix"></div>
+						</div>
+					</div>
+				</div>
+					<div class="benefits w3l">
+						<div class="container">
+							<div class="benefits-grids">
+								<div class="col-md-6 benefits-grid wow fadeInLeft animated animated" data-wow-delay="0.4s">
+									<h3>Преимущества</h3>
+									<div class="benefit-top">
+										<div class="benefit-left">
+											<h4>1</h4>
+										</div>
+										<div class="benefit-right">
+											<p> В нашем зале много пространства. Одновременно могут заниматься большое количество людей.</p>
+										</div>
+										<div class="clearfix"></div>
+									</div>
+									<div class="benefit-top">
+										<div class="benefit-left">
+											<h4>2</h4>
+										</div>
+										<div class="benefit-right">
+											<p> Только новые и современные тренажеры. Заниматься на них одно удовольствие.</p>
+										</div>
+										<div class="clearfix"></div>
+									</div><div class="benefit-top">
+										<div class="benefit-left">
+											<h4>3</h4>
+										</div>
+										<div class="benefit-right">
+											<p> Удобное место расположения. Добраться из любой тоски города не проблема.</p>
+										</div>
+										<div class="clearfix"></div>
+									</div>
+									<div class="benefit-top">
+										<div class="benefit-left">
+											<h4>4</h4>
+										</div>
+										<div class="benefit-right">
+											<p> Команда высококлассных специалистов помогут сделать тренировку эффетивной.</p>
+										</div>
+										<div class="clearfix"></div>
+									</div>
+								</div>
+								<div class="col-md-6 benefits-grid1 wow fadeInRight animated animated" data-wow-delay="0.4s">
+									<form action="#" method="post">
+										<input type="text" name="name" placeholder="ФИО" required>
+										<input type="text" name="email" class="email" placeholder="Email" required>
+										<input type="text" name="phone"  class="phone"placeholder="Номер телефона" required>
+										<textarea name="text area" placeholder="Сообщение"></textarea>
+										<input type="submit" value="Записаться">
+									</form>
+								</div>
+								<div class="clearfix"></div>
+							</div>
+						</div>
+					</div>
 
-  <?php if (isset($_GET['submitted'])) { ?>
-    <h1>The server had accepted:</h1>
-    <p>radio: <?php echo htmlspecialchars($_GET['radio']); ?></p>
-    <p>radio: <?php echo htmlspecialchars($_GET['radio']); ?></p>
-    <p>color: <?php echo htmlspecialchars($_GET['color']); ?></p>
-    <p>date: <?php echo htmlspecialchars($_GET['date']); ?></p>
-    <p>month: <?php echo htmlspecialchars($_GET['month']); ?></p>
-    <p>email: <?php echo htmlspecialchars($_GET['email']); ?></p>
-    <p>number: <?php echo htmlspecialchars($_GET['number']); ?></p>
-    <p>range: <?php echo htmlspecialchars($_GET['range']); ?></p>
-    <p>agreement: <?php echo htmlspecialchars($_GET['box']); ?></p>
-    <p>choise: <?php echo htmlspecialchars($_GET['select']); ?></p>
-    <p>Try again? <a href="/index.php">Yes!</a></p>
-  <?php } ?>
-
-  <?php if (!isset($_GET['submitted'])) { ?>
-  
-  <h1>Play with the HTML form</h1>
-  <form action="/index.php">
-    <input type="hidden" name="submitted" value="yes">
-    <p>
-      <label for="my-radio-yes">radio Yes: </label>
-      <input id="my-radio-yes" name="radio" type="radio" required value="Yes">
-      <label for="my-radio-no">radio No: </label>
-      <input id="my-radio-no" name="radio" type="radio" required value="No"></p>
-      <p><label for="my-color">color: </label>       <input id="my-color" type="color" name="color"></p>
-      <p><label for="my-date">date: </label>         <input id="my-date" type="date" name="date" required></p>
-      <p><label for="my-month">month: </label>       <input id="my-month" type="month" name="month" required></p>
-      <p><label for="my-email">email: </label>       <input id="my-email" type="email" name="email"required></p>
-      <p><label for="my-number">number: </label>     <input id="my-number" type="number" name="number"></p>
-      <p><label for="my-range">range: </label>       <input id="my-range" type="range" min="0" max="10" name="range"></p>
-      <p><label for="my-checkbox">checkbox: </label> <input id="my-checkbox" type="checkbox" name="box"checked></p>
-      <p><label for="my-select">select: </label>     <select id="my-select" size="1"name="select" required>
-      <option value="">---------</option>
-      <option value="1">Первая</option>
-      <option value="2" selected>Вторая</option>
-      </select></p>
-      <textarea id="my-textarea" style="resize:none" placeholder="Введите многострочный текст"></textarea><br>
-
-    <br>
-    <input type="submit" value="Submit">
-    <input type="reset" value="Clear">
-  </form>
-
-  <?php } ?>
-
+			</div>
+		</div>
+	<div class="copy-section wow fadeInDownBig animated animated" data-wow-delay="0.4s">
+		<div class="container">
+				<div class="social-icons">
+					<a href="#"><i class="icon"></i></a>
+					<a href="#"><i class="icon1"></i></a>
+					<a href="#"><i class="icon2"></i></a>
+					<a href="#"><i class="icon3"></i></a>
+				</div>
+			<p>&copy; 2017 BeInShape Gym . All rights reserved | Design by Alia Zhukava</p>
+		</div>
+	</div>
 </body>
 </html>
